934b25d84ef527e1f97eba16c4fb186a5b5fb3ba
Christian Tzolov <christian.tzolov@broadcom.com>
2025-08-23 10:16:17 +0200

    minor
    Signed-off-by: Christian Tzolov <christian.tzolov@broadcom.com>

--- model-context-protocol/mcp-annotations/mcp-annotations-client/src/main/java/org/springframework/ai/mcp/samples/client/ClientMcpHandlers.java
+++ model-context-protocol/mcp-annotations/mcp-annotations-client/src/main/java/org/springframework/ai/mcp/samples/client/ClientMcpHandlers.java
@@ -2,6 +2,8 @@ package org.springframework.ai.mcp.samples.client;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springaicommunity.mcp.annotation.McpElicitation;
 import org.springaicommunity.mcp.annotation.McpLogging;
 import org.springaicommunity.mcp.annotation.McpProgress;
@@ -9,37 +11,47 @@ import org.springaicommunity.mcp.annotation.McpSampling;
 import org.springframework.stereotype.Service;
 
 import io.modelcontextprotocol.spec.McpSchema;
+import io.modelcontextprotocol.spec.McpSchema.CreateMessageRequest;
 import io.modelcontextprotocol.spec.McpSchema.CreateMessageResult;
-import io.modelcontextprotocol.spec.McpSchema.ElicitRequest;
 import io.modelcontextprotocol.spec.McpSchema.ElicitResult;
 import io.modelcontextprotocol.spec.McpSchema.LoggingMessageNotification;
+import io.modelcontextprotocol.spec.McpSchema.ProgressNotification;
 
 @Service
 public class ClientMcpHandlers {
 
+	private static final Logger logger = LoggerFactory.getLogger(ClientMcpHandlers.class);
+
 	@McpProgress(clientId = "server1")
-	public void progress(McpSchema.ProgressNotification progressNotification) {
-		System.out.println("MCP PROGRESS: [" + progressNotification.progressToken() + "] progress: "
-				+ progressNotification.progress() + " total: " + progressNotification.total() + " message: "
-				+ progressNotification.message());
+	public void progressHandler(ProgressNotification progressNotification) {
+		logger.info("MCP PROGRESS: [{}] progress: {} total: {} message: {}",
+				progressNotification.progressToken(), progressNotification.progress(),
+				progressNotification.total(), progressNotification.message());
 	}
 
 	@McpLogging
-	public void logging(LoggingMessageNotification loggingMessage) {
-		System.out.println("MCP LOGGING: [" + loggingMessage.level() + "] " + loggingMessage.data());
+	public void loggingHandler(LoggingMessageNotification loggingMessage) {
+		logger.info("MCP LOGGING: [{}] {}", loggingMessage.level(), loggingMessage.data());
 	}
 
 	@McpSampling
-	public CreateMessageResult sampling(McpSchema.CreateMessageRequest llmRequest) {
+	public CreateMessageResult samplingHandler(CreateMessageRequest llmRequest) {
+
+		logger.info("   MCP SAMPLING REQUEST: {}", llmRequest);
+
 		String userPrompt = ((McpSchema.TextContent) llmRequest.messages().get(0).content()).text();
+
 		String modelHint = llmRequest.modelPreferences().hints().get(0).name();
+
 		return CreateMessageResult.builder()
 				.content(new McpSchema.TextContent("Response " + userPrompt + " with model hint " + modelHint))
 				.build();
 	};
 
 	@McpElicitation
-	public ElicitResult elicit(ElicitRequest request) {
+	public ElicitResult elicitationHandler(McpSchema.ElicitRequest request) {
+		logger.info("MCP ELICITATION REQUEST: {}", request);
+
 		return new ElicitResult(ElicitResult.Action.ACCEPT, Map.of("message", request.message()));
 	}
 
