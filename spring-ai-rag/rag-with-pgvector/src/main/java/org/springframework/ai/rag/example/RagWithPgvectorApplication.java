a06b769a9891090dae8c48fe7436208723e62259
adil <alisnagina@gmail.com>
2025-09-04 16:38:55 +0530

    add controller and service for chat and embedding data into pgvector
    Signed-off-by: adil <alisnagina@gmail.com>

---
+++ spring-ai-rag/rag-with-pgvector/src/main/java/org/springframework/ai/rag/example/RagWithPgvectorApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.ai.rag.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagWithPgvectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagWithPgvectorApplication.class, args);
+	}
+
+}
